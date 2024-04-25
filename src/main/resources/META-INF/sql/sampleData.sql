4132d2ae97ca16965fde9c3327c2230de0bbce34
Juan Manuel Arriaza Rey <jmar0022@red.ujaen.es>
2024-04-25 11:38:33 +0200

    Corrección de errores en la eliminación de actividades

--- src/main/resources/META-INF/sql/sampleData.sql
+++ src/main/resources/META-INF/sql/sampleData.sql
@@ -1,6 +1,7 @@
-insert into Actividad(id,CAPACIDAD,DIASEMANA,FECHA,NOMBRE) values (2,'18','Lunes','2022-03-01 11:40','Pilates');
-insert into Actividad(id,CAPACIDAD,DIASEMANA,FECHA,NOMBRE) values (3,'11','Martes','2022-03-4 19:15','Boxeo');
-insert into Actividad(id,CAPACIDAD,DIASEMANA,FECHA,NOMBRE) values (4,'6','Jueves','2022-03-12 18:40','Crossfit');
+insert into Actividad(id,CAPACIDAD,DIASEMANA,FECHA,NOMBRE) values (2,'16','Lunes','2022-03-01 10:30','Yoga');
+insert into Actividad(id,CAPACIDAD,DIASEMANA,FECHA,NOMBRE) values (3,'18','Lunes','2022-03-01 11:40','Pilates');
+insert into Actividad(id,CAPACIDAD,DIASEMANA,FECHA,NOMBRE) values (4,'11','Martes','2022-03-4 19:15','Boxeo');
+insert into Actividad(id,CAPACIDAD,DIASEMANA,FECHA,NOMBRE) values (5,'6','Jueves','2022-03-12 18:40','Crossfit');
 
 insert into Users(ID,APELLIDOS,CIUDAD,CLAVE,CP,DIRECCION,DNI,EMAIL,FECHANACIMIENTO,NOMBRE,NOMBRE_USUARIO,ROL,TELEFONO) values(1,'Garcia','Madrid','1234','28080','Calle Mayor','12345678A','jperez@red.ujaen.es','1999-02-22','Juan','jmar0022','USER','123456789');
 insert into Users(ID,APELLIDOS,CIUDAD,CLAVE,CP,DIRECCION,DNI,EMAIL,FECHANACIMIENTO,NOMBRE,NOMBRE_USUARIO,ROL,TELEFONO) values(2,'Arriaza','Jaén','12345','23006','Calle Prueba','12345678A','jmar0022@red.ujaen.es','1998-12-17','Juanma','jmar0022','ADMIN','123456789');
