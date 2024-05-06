9cfa8039fe8d869392a25c728f2d43be9209e438
Ignacio Delgado Lobelle <idl00005@red.ujaen.es>
2024-05-06 17:52:24 +0200

    Actualización de la imagen en el lado del cliente implementada

--- src/main/java/controller/cuentaController.java
+++ src/main/java/controller/cuentaController.java
@@ -2,6 +2,7 @@ package controller;
 import com.example.g116.qualifiers.DAOJpaUser;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
+import jakarta.servlet.http.Part;
 import model.dao.UserDAOJpa;
 import model.validator.User;
 import jakarta.faces.application.FacesMessage;
@@ -14,7 +15,14 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Set;
 
@@ -24,6 +32,8 @@ public class cuentaController implements Serializable {
     @Inject @DAOJpaUser
     private UserDAOJpa userDAOJpa;
     private User user;
+    private Part file;
+    private String profileImageUrl = "img/foto_perfil_por_defecto.webp";
     public cuentaController() {
         HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
         user = (User) session.getAttribute("loggedInUser");
@@ -51,5 +61,34 @@ public class cuentaController implements Serializable {
         userDAOJpa.update(user);
 
         return "cuenta?faces-redirect=true";
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Part getFile() {
+        return file;
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
+
+        if (file != null && file.getContentType().startsWith("image/")) {
+            String fileName = Paths.get(file.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+            String directory = "./img"; // Replace with your directory
+            Path path = Paths.get(directory, fileName);
+
+            try (InputStream input = file.getInputStream()) {
+                Files.copy(input, path, StandardCopyOption.REPLACE_EXISTING);
+                this.profileImageUrl = "./img" + fileName; // Replace with your URL path
+            } catch (IOException e) {
+                // Handle the exception
+            }
+        }
     }
 }
